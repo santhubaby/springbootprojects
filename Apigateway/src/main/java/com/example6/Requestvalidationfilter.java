b43b9d496f57d0506100952b394ed2605f49d693
santhoshi <munthasanthoshi@gmail.com>
2026-08-01 08:50:03 +0530

    Apigateway added

---
+++ Apigateway/src/main/java/com/example6/Requestvalidationfilter.java
@@ -0,0 +1,47 @@
+package com.example6;
+
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+
+import reactor.core.publisher.Mono;
+
+
+
+@Component
+public class Requestvalidationfilter implements GlobalFilter {
+	
+	   @Override
+		public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+			System.out.println("filter () called...");
+
+			// Logic to validate the request
+
+			ServerHttpRequest request = exchange.getRequest();
+
+			HttpHeaders headers = request.getHeaders();
+
+			Set<String> keySet = headers.keySet();
+
+			if (!keySet.contains("SecretToken")) {
+				throw new RuntimeException("Invalid Request");
+			}
+
+			List<String> list = headers.get("SecretToken");
+			if (!list.get(0).equals("santhu@123")) {
+				throw new RuntimeException("Invalid Request");
+			}
+
+			return chain.filter(exchange);
+		}
+
+	}
+
